cbb57379ef69e6b7173c83adff77c3df2b964d0c
Pranto408 <prantodatta408@gmail.com>
2026-03-29 02:14:07 +0600

    Final Commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -3,22 +3,33 @@ import Country from '../Country/Country';
 import './Countries.css';
 const Countries = ({ countriesPromise }) => {
   const [visitedCountries, setVisitedCountries] = useState([])
+
+
   const handelVisitedCountries = (country) => {
-    console.log(country)
-    const newVisitedCountry = [...visitedCountries, country];
-    setVisitedCountries(newVisitedCountry);
-  }
+    const exists = visitedCountries.find(
+      (c) => c.name.common === country.name.common,
+    );
+
+    if (exists) {
+      const remaining = visitedCountries.filter(
+        (c) => c.name.common !== country.name.common,
+      );
+      setVisitedCountries(remaining);
+    } else {
+      setVisitedCountries([...visitedCountries, country]);
+    }
+  };
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
   return (
     <div>
       <h2>Total Countries - {countries.length}</h2>
       <h3>Visited Countries - {visitedCountries.length}</h3>
-      <ol>
+      <ul>
         {visitedCountries.map((country) => (
           <li key={country.cca3.cca3}>{country.name.common}</li>
         ))}
-      </ol>
+      </ul>
       <div className="countries">
         {countries.map((country) => (
           <Country
